771f3cdd18f5afefcc563823fb05dfc9afec03e4
Mathias Nguyen <matngu21@csu.fullerton.edu>
2021-03-24 18:43:32 -0700

    Recipe class

--- RecipeApp/RecipeApp/RecipeContainer.swift
+++ RecipeApp/RecipeApp/RecipeContainer.swift
@@ -10,23 +10,27 @@
 import Foundation
 import SwiftUI
 
-class RecipeContainer { //Creating a class for storing recipes, need to read up on docs about storage
+class RecipeContainer {
+    //Creating a class for storing recipes, need to read up on docs about storage
     var RecipeName: String
-    var Description: String? //not every recipe needs a description
+    var Description: String?
+    //An optional because not every recipe needs a description
     var Image: String? {
         return nil
     }
     var Ingredients: [String: String]
-    var Steps = Array<Any>(arrayLiteral: String(), Int?.self) //creates an empty array
-    //and accepts an empty string and an optional int, checks the time in steps I think, **I've no idea this was just spagetti code, might have to fix this later if not working**
+    var Steps = Array<Any>(arrayLiteral: String(), Int?.self)
+    //arrayLiteral creates an empty array and accepts an empty string and an optional int, checks the time in steps I think, **I've no idea this was just spagetti code, might have to fix this later if not working**
     var TotalTime: String {
         return("Time: \(Steps.count)")
+        //Counts the time in the string array from Steps and returns it
     }
     
     init(_ RecipeName: String, _ Description: String, _ Ingredients: [String: String]) {
         self.RecipeName = RecipeName
         self.Description = Description
         self.Ingredients = Ingredients
-    } //init for names and description
+    }
+    //init for names and description
     
 }
