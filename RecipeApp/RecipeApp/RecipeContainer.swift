06b65a01628df9053b86022506783e2a79783747
Mark <Gonzalez>
2021-05-12 13:40:38 -0700

    Moved searchRecipe() outside the recipe class and changed its return value to a non-optional.

--- RecipeApp/RecipeApp/RecipeContainer.swift
+++ RecipeApp/RecipeApp/RecipeContainer.swift
@@ -32,36 +32,37 @@ class RecipeContainer: Codable{
         self.Ingredients = Ingredients
         self.Steps = Steps
     }
-    //Below: Creating a search function that accepts an enum and string to return an array of recipes
-    func searchRecipe(attribute: searchAtt, searchStr: String) -> Array<RecipeContainer>?{
-       //searches through masterlist to match patterns and stores recipes
-        /*let RecipeArray = MasterList
-        for recipe in MasterList{
-            let _recipe = MasterList.RecipeContainer
-        }*/
-        var SearchArray: [RecipeContainer] = []
-        switch attribute {
-        case searchAtt.name:
-            for recipe in MasterList {
-                if (recipe.RecipeName.range(of: searchStr) != nil){
-                    SearchArray.append(recipe)
-                }
+}
+
+//Below: Creating a search function that accepts an enum and string to return an array of recipes
+func searchRecipe(_ attribute: searchAtt,_ searchStr: String) -> Array<RecipeContainer>{
+   //searches through masterlist to match patterns and stores recipes
+    /*let RecipeArray = MasterList
+    for recipe in MasterList{
+        let _recipe = MasterList.RecipeContainer
+    }*/
+    var SearchArray: [RecipeContainer] = []
+    switch attribute {
+    case searchAtt.name:
+        for recipe in MasterList {
+            if (recipe.RecipeName.range(of: searchStr) != nil){
+                SearchArray.append(recipe)
             }
-            return SearchArray
-        case searchAtt.ingredient:
-            for recipe in MasterList {
-                if (recipe.Ingredients.keys.contains(searchStr)) {
-                    SearchArray.append(recipe)
-                }
-            }
-            return SearchArray
-        case searchAtt.time:
-            for recipe in MasterList {
-                if ((recipe.TotalTime.range(of: searchStr)) != nil) {
-                    SearchArray.append(recipe)
-                }
-            }
-            return SearchArray
         }
+        return SearchArray
+    case searchAtt.ingredient:
+        for recipe in MasterList {
+            if (recipe.Ingredients.keys.contains(searchStr)) {
+                SearchArray.append(recipe)
+            }
+        }
+        return SearchArray
+    case searchAtt.time:
+        for recipe in MasterList {
+            if ((recipe.TotalTime.range(of: searchStr)) != nil) {
+                SearchArray.append(recipe)
+            }
+        }
+        return SearchArray
     }
 }
