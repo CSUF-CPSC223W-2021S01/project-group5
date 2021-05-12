8f1db1261aea5fc169fc0fba9f1f7dffbe69583b
Mathias Nguyen <matngu21@csu.fullerton.edu>
2021-05-12 14:03:14 -0700

    Dark mode implementation in progress

--- RecipeApp/RecipeApp/SettingsViewController.swift
+++ RecipeApp/RecipeApp/SettingsViewController.swift
@@ -9,5 +9,23 @@ import UIKit
 
 class SettingsViewController: UIViewController{
     
+    var lightOn = true
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        lightOn = !lightOn
+        
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
